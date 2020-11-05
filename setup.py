6b5a075c00770e022777b39b87815df8b0e900eb
Michael Teti <mtetifau@gmail.com>
2020-11-05 16:15:09 -0500

    stuff for module

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = 'NEMO',
+    version = '0.0.1',
+    description = 'Neural Encoding Models for Ophys Data project module.',
+    author = 'Michael Teti',
+    author_email = 'mteti@fau.edu',
+    packages = ['NEMO'],
+    install_requires = [
+        'numpy<1.19.0',
+        'scipy',
+        'allensdk',
+        'progressbar2',
+        'opencv-python',
+        'sklearn',
+        'matplotlib',
+        'seaborn',
+        'pandas==0.25.3',
+        'h5py<3.0.0',
+        'torch',
+        'urllib3',
+        'chardet',
+        'idna',
+        'jinja2<2.12.0'
+    ]
+)
