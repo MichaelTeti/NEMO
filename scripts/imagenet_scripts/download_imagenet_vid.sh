54d517b7ed7627f177e96ebd716a3b1aeac3146e
Michael Teti <mteti@lanl.gov>
2020-11-16 13:58:41 -0700

    removed the extraction of VID/* files only...takes really long

--- scripts/imagenet_scripts/download_imagenet_vid.sh
+++ scripts/imagenet_scripts/download_imagenet_vid.sh
@@ -4,7 +4,6 @@ wget http://bvisionweb1.cs.unc.edu/ilsvrc2015/ILSVRC2015_VID.tar.gz \
 	-P ../../data/ &&
 
 tar -xvzf ../../data/ILSVRC2015_VID.tar.gz \
-	--directory ../../data/ \
-	ILSVRC2015_VID/Data/VID &&
+	--directory ../../data/ &&
 
 rm ../../data/ILSVRC2015_VID.tar.gz
