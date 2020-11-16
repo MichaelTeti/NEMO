9c60a0e518cc54906a19b0c494b03b6af7fbc02c
Michael Teti <mteti@lanl.gov>
2020-11-16 09:13:11 -0700

    only extracting video frames from tar bc we don't need everything

--- scripts/imagenet_scripts/download_imagenet_vid.sh
+++ scripts/imagenet_scripts/download_imagenet_vid.sh
@@ -1,4 +1,10 @@
 mkdir ../../data/ &&
-wget http://bvisionweb1.cs.unc.edu/ilsvrc2015/ILSVRC2015_VID.tar.gz -P ../../data/ &&
-tar -xvzf ../../data/ILSVRC2015_VID.tar.gz --directory ../../data/ &&
+
+wget http://bvisionweb1.cs.unc.edu/ilsvrc2015/ILSVRC2015_VID.tar.gz \
+	-P ../../data/ &&
+
+tar -xvzf ../../data/ILSVRC2015_VID.tar.gz \
+	--directory ../../data/ \
+	ILSVRC2015_VID/Data/VID &&
+
 rm ../../data/ILSVRC2015_VID.tar.gz
