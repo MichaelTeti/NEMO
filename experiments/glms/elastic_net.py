5de0588173878be430b7c767170f2b229f647d87
Michael Teti <mtetifau@gmail.com>
2020-12-08 12:28:35 -0700

    fixed these shape errors in test

--- experiments/glms/elastic_net.py
+++ experiments/glms/elastic_net.py
@@ -12,7 +12,7 @@ from NEMO.utils.image_utils import read_frames, max_min_scale
 from NEMO.utils.model_utils import create_temporal_design_mat, save_args
 
 
-def train_elastic_net(design_mat, trace_fpaths, save_dir, min_l1_ratio = 1e-6, max_l1_ratio = 1.0, 
+def train_elastic_net(design_mat, trace_dir, save_dir, min_l1_ratio = 1e-6, max_l1_ratio = 1.0, 
                       n_l1_ratios = 6, n_frames_in_time = 9, n_alphas = 50, max_iter = 5000, 
                       n_jobs = 4, write_mse_plots = False, write_rf_images = False, args_to_write = None):
     
@@ -38,6 +38,11 @@ def train_elastic_net(design_mat, trace_fpaths, save_dir, min_l1_ratio = 1e-6, m
     # create list of l1_ratios to try
     l1_ratios = np.linspace(min_l1_ratio, max_l1_ratio, n_l1_ratios)
     print('[INFO] L1 RATIOS: {}'.format(l1_ratios))
+
+    # get a list of all cell traces in trace_dir
+    trace_fpaths = [os.path.join(trace_dir, f) for f in os.listdir(trace_dir)]
+    assert(all([os.path.splitext(fpath)[1] == '.txt' for fpath in trace_fpaths])), \
+        'All files in trace_dir must be .txt files made by the get_trial_averaged_responses.py script.'
 
     for cell_num, fpath in enumerate(trace_fpaths):
         # pull cell ID from filename for later saving of results
@@ -122,7 +127,7 @@ def test_elastic_net(design_mat, trace_dir, save_dir, n_frames_in_time = 9):
     with h5py.File(os.path.join(save_dir, 'strfs.h5'), 'r') as h5file:
         # standardize with training statistics saved in .h5 file
         mean_vec, std_vec = h5file['mean_vec'][()], h5file['std_vec'][()]
-        assert mean_vec.shape[1] == design_mat.shape[1]
+        assert mean_vec.size == design_mat.shape[1]
         design_mat = (design_mat - mean_vec) / std_vec
         
         # get cell ids in the h5file
@@ -145,13 +150,14 @@ def test_elastic_net(design_mat, trace_dir, save_dir, n_frames_in_time = 9):
             
             # read in the coefficients and reshape for matrix multiply
             strf = h5file[cell_id][()]
-            strf = strf[:, None]
+            strf = strf.flatten()[:, None]
+            assert design_mat.shape[1] == strf.shape[0]
             
             # predict traces 
             traces_pred = np.matmul(design_mat, strf)
             
             # save the data
-            results = pd.DataFrame({'true': traces, 'pred': traces_pred})
+            results = pd.DataFrame({'true': traces, 'pred': np.squeeze(traces_pred)})
             results.to_csv(os.path.join(results_dir, cell_id + '.txt'), index = False)
             
             
@@ -243,11 +249,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # get a list of all cell traces in trace_dir
-    trace_fpaths = [os.path.join(args.trace_dir, f) for f in os.listdir(args.trace_dir)]
-
-    assert(all([os.path.splitext(fpath)[1] == '.txt' for fpath in trace_fpaths])), \
-        'All files in trace_dir must be .txt files made by the get_trial_averaged_responses.py script.'
 
     # get array of stimulus frames
     stimulus = read_frames(args.stimulus_dir, gray = True)
@@ -261,7 +262,7 @@ if __name__ == '__main__':
     if args.mode == 'train':
         train_elastic_net(
             design_mat = design_mat,
-            trace_fpaths = trace_fpaths,
+            trace_dir = args.trace_dir,
             save_dir = args.save_dir,
             min_l1_ratio = args.min_l1_ratio,
             max_l1_ratio = args.max_l1_ratio,
@@ -276,8 +277,8 @@ if __name__ == '__main__':
         )
     elif args.mode == 'test':
         test_elastic_net(
-            design_mat,
-            trace_dir,
-            save_dir,
+            design_mat = design_mat,
+            trace_dir = args.trace_dir,
+            save_dir = args.save_dir,
             n_frames_in_time = args.n_frames_in_time
         )
