be500bb56ea8469857ed5dfb8dce0fc018f3fe82
Michael A. Teti <mtetifau@gmail.com>
2020-12-22 08:10:45 -0500

    had to change bc mean_activations were not returning full act tensor

--- NEMO/visualization/lca.py
+++ NEMO/visualization/lca.py
@@ -248,7 +248,21 @@ def get_mean_sparsity(model_activity_fpath, openpv_path = '/home/mteti/OpenPV/ml
             consisting of the original index of the neuron with that sorted mean sparsity.
     '''
     
-    acts, _, _ = get_mean_activations(model_activity_fpath, openpv_path = openpv_path)
+    # add OpenPV matlab utility dir to octave
+    octave.addpath(openpv_path)
+    
+    # Read in the activity file and get the number of batch samples
+    # Should be a list of length batch size, where each item in the list is an array 
+    # of shape (input_width / stride x) x (input height / stride y) x n_neurons
+    # with the activations for that particular batch sample
+    act_data = octave.readpvpfile(model_activity_fpath)
+    n_batch = len(act_data)
+    
+    # concatenate across batch samples to produce a single array of shape 
+    # batch size x (input width / stride x) x (input height / stride y) x n_neurons
+    acts = np.concatenate([act_data[b]['values'][None, ...] for b in range(n_batch)], 0)
+    
+    # calculate mean sparsity and sort
     mean_sparsity = np.mean(acts == 0.0, (0, 1, 2))
     inds = list(range(mean_sparsity.size))
     sorted_inds = [ind for _,ind in sorted(zip(mean_sparsity, inds), reverse = True)]
