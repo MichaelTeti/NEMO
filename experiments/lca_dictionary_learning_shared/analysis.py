27e180fa61c7e36b26b2623fa23a2e6d8b8d8aac
Michael Teti <mtetifau@gmail.com>
2020-12-22 10:10:16 -0700

    working as desired

--- experiments/lca_dictionary_learning_shared/analysis.py
+++ experiments/lca_dictionary_learning_shared/analysis.py
@@ -1,47 +1,62 @@
 from argparse import ArgumentParser
 import os
-import sys
 
-from oct2py import octave 
+import matplotlib.pyplot as plt
 
-from NEMO.utils.plot_probes import (
-    plot_energy,
-    plot_adaptive_timescales,
-    plot_l2,
-    plot_firm_thresh
+from NEMO.visualization.lca import (
+    get_mean_activations,
+    get_mean_sparsity,
+    view_complex_cell_strfs,
+    get_percent_neurons_active,
+    plot_objective_probes,
+    plot_adaptive_timescale_probes,
+    view_reconstructions
 )
 
 
 parser = ArgumentParser()
 parser.add_argument(
-    '--openpv_path',
-    type = str,
-    required = True,
-    help = 'Path to the OpenPV/mlab/util directory.'
-)
-parser.add_argument(
-    '--ckpt_dir',
-    required = True,
+    'ckpt_dir',
     type = str,
     help = 'Path to the OpenPV checkpoint.'
 )
 parser.add_argument(
-    '--save_dir',
-    required = True,
+    'save_dir',
     type = str,
     help = 'Where to save these analyses and write out plots.'
 )
 parser.add_argument(
-    '--model_layer_name',
+    '--openpv_path',
     type = str,
-    default = 'S1',
-    help = 'The name of the model layer.'
+    default = '/home/mteti/OpenPV/mlab/util',
+    help = 'Path to *OpenPV/mlab/util',
+)
+parser.add_argument(
+    '--no_features',
+    action = 'store_true',
+    help = 'If specified, will not plot the features.'
+)
+parser.add_argument(
+    '--no_recons',
+    action = 'store_true',
+    help = 'If specified, will not plot the reconstructions and inputs.'
+)
+parser.add_argument(
+    '--no_probes',
+    action = 'store_true',
+    help = 'If specified, will not plot the probes.'
+)
+parser.add_argument(
+    '--no_activity',
+    action = 'store_true',
+    help = 'If specified, will not plot mean activations, mean sparsity, \
+        or mean activity.'
 )
 
 # visualize features
 feat_args = parser.add_argument_group(
     'feature visualization',
-    description = 'Arguments for the viz_shared_vid_weights.m function.'
+    description = 'Arguments for the view_complex_cell_strfs function.'
 )
 feat_args.add_argument(
     '--weight_fpath_key',
@@ -50,55 +65,67 @@ feat_args.add_argument(
         directory, since there may be multiple in the same one for \
         some models.'
 )
-feat_args.add_argument(
-    '--clip_frame_0',
-    action = 'store_true',
-    help = 'If specified, will clip the frame 0 feature values with \
-        the min/max of the features for all other frames. This has to \
-        do with the way we initialize the connection values on video \
-        stimuli, and it is helpful to see changes in the first few runs \
-        after initialization before the weights from all other frames \
-        catch up with frame 0.'
-)
-feat_args.add_argument(
-    '--sorted_feats',
-    action = 'store_true',
-    help = 'If specified, will display the features in descending order \
-        of activation from top left to bottom right.'
-)
 
 # visualize reconstructions
 rec_args = parser.add_argument_group(
     'reconstruction visualization',
-    description = 'The arguments for the viz_reconstructions.m function.'
+    description = 'The arguments for the view_reconstructions function.'
 )
 rec_args.add_argument(
     '--rec_layer_key',
     type = str,
-    default = 'Frame*Recon_A.pvp',
+    default = 'Frame[0-9]Recon_A.pvp',
     help = 'A key to help find the .pvp files written out by the reconstruction \
-        layer in the checkpoint directory (e.g. "Frame*Recon_A.pvp" where the \
-        asterisk represents the frame number).'
+        layer in the checkpoint directory.'
 )
 rec_args.add_argument(
     '--input_layer_key',
     type = str,
-    default = 'Frame*_A.pvp',
+    default = 'Frame[0-9]_A.pvp',
     help = 'A key to help find the .pvp files written out by the input layer \
-        in the checkpoint directory (e.g. "Frame*_A.pvp" where the asterisk \
-        represents the frame number).'
+        in the checkpoint directory.'
+)
+
+# plotting mean activations and mean sparsity 
+mean_act_args = parser.add_argument_group(
+    'mean activations',
+    description = 'The arguments to plot the mean activations and mean sparstiy.'
+)
+mean_act_args.add_argument(
+    '--model_activity_fpath',
+    required = True,
+    type = str,
+    help = 'The path to the <model_layer_name>_A.pvp file.'
 )
 
 # plotting the number of neurons active at once
 n_active_args = parser.add_argument_group(
     'number active',
-    description = 'The arguments for the plot_num_active.m function.'
+    description = 'The arguments for the plot_num_neurons_active function.'
 )
 n_active_args.add_argument(
     '--model_write_fpath',
     required = True,
     type = str,
-    help = 'Path to the <model_layer>.pvp file.'
+    help = 'Path to the <model_layer_name>.pvp file.'
+)
+n_active_args.add_argument(
+    '--n_neurons',
+    type = int,
+    default = 256,
+    help = 'The number of dictionary features.'
+)
+n_active_args.add_argument(
+    '--feat_map_h',
+    type = int,
+    default = 32,
+    help = 'The height of the feature map grid of activations.'
+)
+n_active_args.add_argument(
+    '--feat_map_w',
+    type = int,
+    default = 64,
+    help = 'The width of the feature map grid of activations.'
 )
 
 # probe arguments 
@@ -120,19 +147,19 @@ probe_args.add_argument(
         (e.g. "*L2Probe*").'
 )
 probe_args.add_argument(
-    '--firm_thresh_key',
+    '--firm_thresh_probe_key',
     type = str,
     default = 'S1FirmThresh*',
     help = 'A key to help filter out the Firm Thresh files in probe_dir.'
 )
 probe_args.add_argument(
-    '--energy_key',
+    '--energy_probe_key',
     type = str,
     default = 'Energy*',
     help = 'A key to help filter out the Energy files in the probe_dir.'
 )
 probe_args.add_argument(
-    '--adaptive_ts_key',
+    '--adaptive_ts_probe_key',
     type = str,
     default = 'Adaptive*',
     help = 'A key to help filter out the adaptive timescale probes in probe_dir.'
@@ -148,68 +175,103 @@ probe_args.add_argument(
     type = int,
     help = 'How many display periods to show starting from the end and going backward.'
 )
+probe_args.add_argument(
+    '--plot_individual_probes',
+    action = 'store_true',
+    help = 'If specified, will plot each probe .txt file individually as well as \
+        moving average.'
+)
 
 args = parser.parse_args()
 
 # make the save directory
 os.makedirs(args.save_dir, exist_ok = True)
 
-# run the octave scripts first
-octave.addpath(args.openpv_path)
-octave.addpath('../../NEMO/utils/')
+if not args.no_features:
+    print('[INFO] WRITING FEATURES...')
+    view_complex_cell_strfs(
+        ckpt_dir = args.ckpt_dir,
+        write_fpath = os.path.join(args.save_dir, 'features.gif'),
+        weight_file_key = args.weight_fpath_key,
+        activity_fpath = args.model_activity_fpath,
+        openpv_path = args.openpv_path
+    )
 
-print('[INFO] WRITING FEATURES...')
-feature_save_path = os.path.join(
-    args.save_dir,
-    'weights_clipped.gif' if args.clip_frame_0 else 'weights.gif'
-)
-octave.viz_shared_vid_weights(
-    args.ckpt_dir,
-    os.path.join(args.save_dir, 'weights.gif'),
-    args.weight_fpath_key,
-    args.clip_frame_0,
-    args.sorted_feats,
-    os.path.join(args.ckpt_dir, args.model_layer_name + '_A.pvp')
-)
-
-print('[INFO] WRITING INPUTS AND RECONSTRUCTIONS...')
-octave.viz_reconstructions(
-    args.ckpt_dir,
-    os.path.join(args.save_dir, 'Inputs_and_Recons'),
-    args.rec_layer_key,
-    args.input_layer_key
-)
-
-octave.plot_num_active(args.model_write_fpath)
-octave.get_mean_acts(os.path.join(args.ckpt_dir, args.model_layer_name + '_A.pvp'))
+if not args.no_recons:
+    print('[INFO] WRITING INPUTS AND RECONSTRUCTIONS...')
+    view_reconstructions(
+        ckpt_dir = args.ckpt_dir,
+        save_dir = os.path.join(args.save_dir, 'Inputs_and_Recons'),
+        recon_layer_key = args.rec_layer_key,
+        input_layer_key = args.input_layer_key,
+        openpv_path = args.openpv_path
+    )
 
 # plotting probes below
-print('[INFO] PLOTTING PROBES...')
-plot_energy(
-    args.probe_dir, 
-    os.path.join(args.save_dir, 'EnergyProbe'), 
-    args.energy_key, 
-    display_period = args.display_period_length,
-    n_display_periods = args.n_display_periods
-)
-plot_adaptive_timescales(
-    args.probe_dir, 
-    os.path.join(args.save_dir, 'AdaptiveTimescaleProbe'), 
-    args.adaptive_ts_key,
-    display_period = args.display_period_length,
-    n_display_periods = args.n_display_periods
-)
-plot_firm_thresh(
-    args.probe_dir, 
-    os.path.join(args.save_dir, 'FirmThreshProbe'), 
-    args.firm_thresh_key,
-    display_period = args.display_period_length,
-    n_display_periods = args.n_display_periods
-)
-plot_l2(
-    args.probe_dir,
-    os.path.join(args.save_dir, 'L2Probe'),
-    args.l2_probe_key,
-    display_period = args.display_period_length,
-    n_display_periods = args.n_display_periods
-)
+if not args.no_probes:
+    print('[INFO] PLOTTING PROBES...')
+    plot_objective_probes(
+        probe_dir = args.probe_dir, 
+        save_dir = os.path.join(args.save_dir, 'EnergyProbe'), 
+        probe_type = 'energy',
+        probe_key = args.energy_probe_key, 
+        display_period = args.display_period_length,
+        n_display_periods = args.n_display_periods,
+        plot_individual = args.plot_individual_probes
+    )
+    plot_objective_probes(
+        probe_dir = args.probe_dir, 
+        save_dir = os.path.join(args.save_dir, 'L2Probe'), 
+        probe_type = 'l2',
+        probe_key = args.l2_probe_key, 
+        display_period = args.display_period_length,
+        n_display_periods = args.n_display_periods,
+        plot_individual = args.plot_individual_probes
+    )
+    plot_objective_probes(
+        probe_dir = args.probe_dir, 
+        save_dir = os.path.join(args.save_dir, 'FirmThreshProbe'), 
+        probe_type = 'firm_thresh',
+        probe_key = args.firm_thresh_probe_key, 
+        display_period = args.display_period_length,
+        n_display_periods = args.n_display_periods,
+        plot_individual = args.plot_individual_probes
+    )
+    
+    if args.plot_individual_probes:    
+        plot_adaptive_timescale_probes(
+            probe_dir = args.probe_dir,
+            save_dir = os.path.join(args.save_dir, 'AdaptiveTimescaleProbe'),
+            probe_key = args.adaptive_ts_probe_key,
+            display_period = args.display_period_length,
+            n_display_periods = args.n_display_periods
+        )
+
+if not args.no_activity:
+    # mean acts, mean sparsity, and number active
+    mean, se, _ = get_mean_activations(args.model_activity_fpath, openpv_path = args.openpv_path)
+    plt.errorbar(x = list(range(mean.size)), y = mean, yerr = se)
+    plt.xlabel('Neuron Index')
+    plt.ylabel('Mean Activation +/- 1 SE')
+    plt.savefig(os.path.join(args.save_dir, 'mean_activations.png'), bbox_inches = 'tight')
+    plt.close()
+
+    mean_sparsity, _ = get_mean_sparsity(args.model_activity_fpath, openpv_path = args.openpv_path)
+    plt.plot(mean_sparsity)
+    plt.xlabel('Neuron Index')
+    plt.ylabel('Mean Sparsity')
+    plt.savefig(os.path.join(args.save_dir, 'mean_sparsity.png'), bbox_inches = 'tight')
+    plt.close()
+
+    mean_active, se_active = get_percent_neurons_active(
+        args.model_write_fpath, 
+        args.n_neurons,
+        args.feat_map_h,
+        args.feat_map_w,
+        openpv_path = args.openpv_path
+    )
+    plt.errorbar(x = list(range(mean_active.size)), y = mean_active, yerr = se_active)
+    plt.xlabel('Display Period Number')
+    plt.ylabel('Mean Percent Active Over Batch +/- 1 SE')
+    plt.savefig(os.path.join(args.save_dir, 'mean_percent_active.png'), bbox_inches = 'tight')
+    plt.close()
