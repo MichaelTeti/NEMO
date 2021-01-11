007c93b849b33a3b5ff493135d05f2a8465449ca
Michael A. Teti <mtetifau@gmail.com>
2021-01-11 13:47:42 -0500

    indentation was off

--- scripts/install_openpv.sh
+++ scripts/install_openpv.sh
@@ -5,21 +5,21 @@ if [ ! -z "$1" ]; then
         cwd=$(pwd) &&
         cd $1 &&
         git clone \
-		    --single-branch \
-		    --branch develop \
-		    --recursive \
-		    --recurse-submodules \
-		    https://github.com/PetaVision/OpenPV.git &&
-        cd OpenPV && 
-	    mkdir build && 
-	    cd build && 
-	    cmake .. \
-		    -DPV_BUILD_SHARED=ON \
-		    -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF \
-		    -DCUDA_NVCC_FLAGS="-Xcompiler -fPIC" && 
-	    make -j $(nproc) && 
-	    ctest &&
-        cd $cwd
+		--single-branch \
+		--branch develop \
+		--recursive \
+		--recurse-submodules \
+		https://github.com/PetaVision/OpenPV.git &&
+	cd OpenPV && 
+	mkdir build && 
+	cd build && 
+	cmake .. \
+		-DPV_BUILD_SHARED=ON \
+		-DCUDA_USE_STATIC_CUDA_RUNTIME=OFF \
+		-DCUDA_NVCC_FLAGS="-Xcompiler -fPIC" && 
+	make -j $(nproc) && 
+	ctest &&
+	cd $cwd
 
     else
     
