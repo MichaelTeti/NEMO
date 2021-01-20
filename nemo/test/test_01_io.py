9d478b90b90c481c2fa4779fcc5c514f33221f24
Michael Teti <mtetifau@gmail.com>
2021-01-20 11:02:59 -0500

    add valueerror check for loading trace array

--- nemo/test/test_01_io.py
+++ nemo/test/test_01_io.py
@@ -14,6 +14,7 @@ import numpy as np
 
 from nemo.data.io import (
     compile_trial_avg_traces,
+    load_trial_avg_trace_array,
     read_frames,
     read_h5_as_array,
     save_vid_array_as_frames,
@@ -97,6 +98,26 @@ class TestIO(unittest.TestCase):
             self.assertAlmostEqual(np.sum(traces.to_numpy() - write), 0.0, places = 12)
 
 
+    def test_load_trial_avg_trace_array(self):
+        write = np.random.randn(10)
+
+        with TemporaryDirectory() as tmp_dir:
+            write_fpath = os.path.join(tmp_dir, 'cellID_0.txt')
+
+            with open(write_fpath, 'w') as f:
+                writer = csv.writer(f)
+                writer.writerow(['0{}.png'.format(j) for j in range(10)])
+                writer.writerow(list(write))
+                
+            for i in range(1, 10):
+                read = load_trial_avg_trace_array(write_fpath, n_frames_in_time = i)
+                self.assertAlmostEqual(np.sum(write[i-1:] - read), 0.0, places = 12)
+
+                
+    def test_load_trial_avg_trace_array_ValueError(self):
+        write = np.random.randn(10)
+        for i in range(-10, 1):
+            self.assertRaises(ValueError, load_trial_avg_trace_array, write, n_frames_in_time = i)
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2)
