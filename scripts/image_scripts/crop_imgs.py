0db72398d1be233b7b02b7187d023190f9baa7e4
Michael Teti <mtetifau@gmail.com>
2021-01-22 11:09:19 -0500

    forgot to remove this

--- scripts/image_scripts/crop_imgs.py
+++ scripts/image_scripts/crop_imgs.py
@@ -42,7 +42,6 @@ fpaths = get_fpaths_in_dir(args.data_dir_parent, key = args.key)
 save_fpaths = add_string_to_fpaths(fpaths, '_cropped')
 save_fpaths = change_file_exts(save_fpaths, '.png')
 for fpath in save_fpaths: os.makedirs(os.path.split(fpath)[0], exist_ok = True)
-fpaths_and_save_fpaths = list(zip(fpaths, save_fpaths))
 multiproc(
     func = read_crop_write,
     iterator_keys = ['read_fpaths', 'write_fpaths'],
--- scripts/image_scripts/downsample_imgs.py
+++ scripts/image_scripts/downsample_imgs.py
@@ -47,7 +47,6 @@ fpaths = get_fpaths_in_dir(args.data_dir_parent, key = args.key)
 save_fpaths = add_string_to_fpaths(fpaths, '_downsampled')
 save_fpaths = change_file_exts(save_fpaths, '.png')
 for fpath in save_fpaths: os.makedirs(os.path.split(fpath)[0], exist_ok = True)
-fpaths_and_save_fpaths = list(zip(fpaths, save_fpaths))
 multiproc(
     read_downsample_write,
     iterator_keys = ['read_fpaths', 'write_fpaths'],
