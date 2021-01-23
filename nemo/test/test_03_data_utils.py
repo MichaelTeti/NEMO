a5bbcc635c6515c9a62e62ba989e231cfefac93a
Michael Teti <mtetifau@gmail.com>
2021-01-23 18:57:52 -0500

    add tests for rest of data.utils.py

--- nemo/test/test_03_data_utils.py
+++ nemo/test/test_03_data_utils.py
@@ -11,8 +11,12 @@ from tempfile import TemporaryDirectory
 import unittest
 
 import cv2
+import numpy as np
 
 from nemo.data.utils import (
+    add_string_to_fpaths,
+    change_file_exts,
+    get_fpaths_in_dir,
     multiproc 
 )
 
@@ -105,6 +109,82 @@ class TestDataUtils(unittest.TestCase):
                 with open(fpath, 'r') as f:
                     data = list(csv.reader(f))[0]
                     self.assertEqual(int(data[0]), file_num)
+
+
+    def test_change_file_exts(self):
+        fpaths = [
+            os.path.join('{}'.format(i), '{}.test.jpg'.format(j)) 
+            for i in range(10) 
+            for j in range(10)
+        ]
+        fpaths_png = change_file_exts(fpaths)
+        
+        for fpath, fpath_png in zip(fpaths, fpaths_png):
+            fpath, ext = os.path.splitext(fpath)
+            fpath_png, ext_png = os.path.splitext(fpath_png)
+            self.assertEqual(fpath, fpath_png)
+            self.assertEqual(ext_png, '.png')
+
+
+    def test_get_fpaths_in_dir(self):
+        imgs = np.zeros([10 * 10 * 10, 32, 64])
+
+        with TemporaryDirectory() as tmp_dir:
+            fpaths = [
+                os.path.join(tmp_dir, '{}'.format(i), '{}'.format(j), '{}.test.jpg'.format(k)) 
+                for i in range(10) 
+                for j in range(10)
+                for k in range(10)
+            ]
+
+            for fpath, img in zip(fpaths, imgs):
+                os.makedirs(os.path.split(fpath)[0], exist_ok = True)
+                cv2.imwrite(fpath, img)
+
+            fpaths_found = get_fpaths_in_dir(tmp_dir)
+            
+            for fpath_found in fpaths_found:
+                self.assertTrue(fpath_found in fpaths)
+
+
+    def test_get_fpaths_in_dir_key(self):
+        with TemporaryDirectory() as tmp_dir:
+
+            n_find = 0
+            fpaths = []
+            for i in range(10):
+                for j in range(10):
+                    keyword = 'find' if (i % 2 == 0 and j % 2 == 0) else 'miss'
+                    n_find = n_find + 1 if keyword == 'find' else n_find
+                    fpath = os.path.join(
+                        tmp_dir,
+                        '{}'.format(i),
+                        '{}_{}'.format(j, keyword),
+                        'test.png'
+                    )
+                    fpaths.append(fpath)
+                    os.makedirs(os.path.split(fpath)[0], exist_ok = True)
+                    cv2.imwrite(fpath, np.random.randn(32, 64))
+
+            fpaths_found = get_fpaths_in_dir(tmp_dir, key = 'find')
+            
+            for fpath_found in fpaths_found:
+                self.assertTrue(fpath_found in fpaths)
+
+
+    def test_add_string_to_fpaths(self):
+        fpaths = [
+            os.path.join('{}'.format(i), '{}'.format(j), '{}.test.jpg'.format(k)) 
+            for i in range(10) 
+            for j in range(10)
+            for k in range(10)
+        ]
+        fpaths_renamed = add_string_to_fpaths(fpaths, 'test_string')
+
+        for fpath_renamed in fpaths_renamed:
+            fpath_renamed_dir, fpath_renamed_fname = os.path.split(fpath_renamed)
+            rename_undone = os.path.join(fpath_renamed_dir.split('test_string')[0], fpath_renamed_fname)
+            self.assertTrue(rename_undone in fpaths)
 
 
 
