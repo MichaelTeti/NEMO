7ac7bd762f70894cf8c2b52c67ab809ed80d4923
Michael Teti <mtetifau@gmail.com>
2021-02-03 22:04:48 -0500

    changed  this bc using dff now

--- nemo/data/preprocess/trace.py
+++ nemo/data/preprocess/trace.py
@@ -9,10 +9,9 @@ def normalize_traces(traces):
         traces (np.ndarray): The array of unscaled fluorescence trace values.
         
     Returns:
-        traces_scaled (np.ndarray): The array of fluorescence traces with zero mean and range [-1, 1].
+        traces_scaled (np.ndarray): The array of fluorescence traces with range [-1, 1].
     '''
     
-    traces -= np.mean(traces)
-    traces /= np.amax(np.absolute(traces))
+    traces /= np.amax(np.absolute(traces), 0)
     
     return traces
