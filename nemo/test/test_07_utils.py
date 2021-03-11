3eb5c9cd7b4741990fd8c8be67c9eb8f3e0e4712
Michael Teti <mtetifau@gmail.com>
2021-03-11 11:50:47 -0500

    multithreading function

--- nemo/test/test_07_utils.py
+++ nemo/test/test_07_utils.py
@@ -3,7 +3,7 @@
 
 import unittest 
 
-from nemo.utils import multiproc
+from nemo.utils import multiproc, multithread
 
 
 
@@ -70,6 +70,70 @@ class TestUtils(unittest.TestCase):
         y = list(range(1, 101))
         z = list(range(5))
         result = multiproc(
+            func = multiply2,
+            iterator_keys = ['x', 'y'],
+            n_procs = 10,
+            x = x,
+            y = y,
+            z = z
+        )
+
+        self.assertTrue(all([rv == zv for rv, zv in zip(result, [z for _ in range(100)])]))
+
+
+    def test_multithread_ValueError_iterables_not_equal_len(self):
+        '''
+        Check designated iterables have equal length.
+        '''
+
+        def test_func(number1, number2):
+            addition = number1 + number2
+
+        with self.assertRaises(ValueError):
+            multithread(
+                test_func,
+                ['number1', 'number2'],
+                n_workers = 10,
+                number1 = list(range(100)),
+                number2 = list(range(10))
+            )     
+
+
+    def test_multithread_n_workers_geq_items(self):
+
+        x = list(range(50))
+        y = list(range(1, 51))
+        result = multithread(
+            func = multiply,
+            iterator_keys = ['x', 'y'],
+            n_procs = 100,
+            x = x,
+            y = y
+        )
+
+        self.assertTrue(all([rv == xv * yv for xv, yv, rv in zip(x, y, result)]))
+
+
+    def test_multithread_n_workers_lt_items(self):
+
+        x = list(range(100))
+        y = list(range(1, 101))
+        result = multithread(
+            func = multiply,
+            iterator_keys = ['x', 'y'],
+            n_procs = 10,
+            x = x,
+            y = y
+        )
+
+        self.assertTrue(all([rv == xv * yv for xv, yv, rv in zip(x, y, result)]))
+
+
+    def test_multithread_non_iterable_arg_list(self):
+        x = list(range(100))
+        y = list(range(1, 101))
+        z = list(range(5))
+        result = multithread(
             func = multiply2,
             iterator_keys = ['x', 'y'],
             n_procs = 10,
--- nemo/utils.py
+++ nemo/utils.py
@@ -1,4 +1,5 @@
 from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 def kwargs_pool(func, kwargs):
@@ -7,7 +8,7 @@ def kwargs_pool(func, kwargs):
     return func(**kwargs)
 
 
-def multiproc(func, iterator_keys, n_procs = 4, **kwargs):
+def multiproc(func, iterator_keys, n_procs = None, **kwargs):
     '''
     A general function to use multiprocessing. In this repo, it is mainly used with functions 
     which process and write data where the processing is CPU-bound. 
@@ -41,6 +42,44 @@ def multiproc(func, iterator_keys, n_procs = 4, **kwargs):
         args.append((func, kwargs_copy))
 
     with Pool(n_procs) as pool:
+        result = pool.starmap(kwargs_pool, args)
+
+    return result
+
+
+def multithread(func, iterator_keys, n_procs = None, **kwargs):
+    '''
+    A general function to use multithreading for IO-bound operations. 
+
+    Args:
+        func (function): A previously defined function.
+        iterator_keys (str): A list of keys in kwargs whose values are the iterables to divide up
+            during multiprocessing. All iterables in kwargs indicated by iterator_keys should have 
+            the same number of elements. kwargs not indicated by iterator_keys will not be
+            divided up during multiprocessing, and instead will be replicated for every function call.
+        n_procs (int): The number of processes to use. Be careful with this.
+        kwargs: keyword arguments to func. The items specified by iterator_keys should be iterables.
+
+    Returns:
+        result: Whatever func returns given kwargs.
+    '''
+
+    # get length of iterables
+    n_inputs = len(kwargs[iterator_keys[0]])
+    if not all([len(kwargs[key]) == n_inputs for key in iterator_keys]):
+        raise ValueError('Iterables corresponding to iterator_keys should all have same length.')
+    
+    # create the arg list for starmap
+    args = []
+    for input_num in range(n_inputs):
+        kwargs_copy = kwargs.copy()
+
+        for key in iterator_keys:
+            kwargs_copy[key] = kwargs_copy[key][input_num]
+            
+        args.append((func, kwargs_copy))
+
+    with ThreadPool(n_procs) as pool:
         result = pool.starmap(kwargs_pool, args)
 
     return result
