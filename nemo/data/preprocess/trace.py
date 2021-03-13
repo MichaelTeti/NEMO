018adad040bc8bb96f6908a55debd1d788071b5e
Michael Teti <mtetifau@gmail.com>
2021-03-13 14:46:43 -0500

    functions to compute trial avg and aggregate dfs

--- nemo/data/preprocess/trace.py
+++ nemo/data/preprocess/trace.py
@@ -1,4 +1,7 @@
+from functools import reduce
+
 import numpy as np
+import pandas as pd
 
 
 def normalize_traces(traces):
@@ -15,3 +18,64 @@ def normalize_traces(traces):
     traces /= np.amax(np.absolute(traces), 0)
     
     return traces
+
+
+def compute_trial_avgs(df):
+    '''
+    Computes average dff over repeated presentations.
+
+    Args:
+        df (pd.DataFrame): Dataframe with at least stimulus, frame, and dff
+            columns.
+    
+    Returns:
+        trial_avg_df (pd.DataFrame): Dataframe where dff is now averaged over
+            repeated presentations of the same frame.
+    '''
+
+    try:
+        df = df.groupby(['stimulus', 'frame']).dff.mean().reset_index()
+    except AttributeError:
+        print('df must have a column named dff.')
+        raise 
+    except KeyError:
+        print('df must have columns named stimulus and frame.')
+        raise 
+    else:
+        df = df.sort_values(by = ['stimulus', 'frame'])
+
+    return df
+
+
+def aggregate_cell_data(dfs, cell_ids = None, keep_cols = None):
+    '''
+    Concatenates dfs.
+
+    Args:
+        dfs (list): List of pd.DataFrame, which all should have the same columns.
+        cell_ids (list): Cell IDs corresponding to dfs. This will be used to rename
+            each one's columns when concatenating dfs because we can't have the same
+            column names.
+        keep_cols (list): If a column name is in keep_cols, the column will kept
+            separate after the merge, whereas columns not in keep_cols will be 
+            merged into one column across dataframes.
+
+    Returns:
+        merged_df (pd.DataFrame): Merged dataframe.
+    '''
+
+    if cell_ids:
+        if len(dfs) != len(cell_ids):
+            raise ValueError('dfs is of different length than cell_ids')
+    else:
+        cell_ids = [str(num) for num in range(len(dfs))]
+
+
+    # also need to rename each column we are interested in bc we can't have the same name
+    if keep_cols is not None:
+        dfs = [
+            df.rename(columns = dict(zip(keep_cols, [cell_id + '_' + col for col in keep_cols]))) 
+            for df, cell_id in zip(dfs, cell_ids)
+        ]
+
+    return reduce(lambda left, right: pd.merge(left, right, how = 'outer'), dfs)
--- nemo/test/test_04_preprocess_trace.py
+++ nemo/test/test_04_preprocess_trace.py
@@ -5,8 +5,13 @@ Unit tests for the classes in data.preprocess.trace.py
 import unittest
 
 import numpy as np
+import pandas as pd
 
-from nemo.data.preprocess.trace import normalize_traces
+from nemo.data.preprocess.trace import (
+    aggregate_cell_data,
+    compute_trial_avgs,
+    normalize_traces
+)
 
 
 class TestTracePreprocess(unittest.TestCase):
@@ -54,6 +59,145 @@ class TestTracePreprocess(unittest.TestCase):
         for i in range(10):
             self.assertAlmostEqual(norm_max[i], 1.0, places = 12)
 
+
+    def test_compute_trial_avgs_AttributeError(self):
+        df = pd.DataFrame({'stimulus': [0, 1], 'frame': [1, 2]})
+        
+        with self.assertRaises(AttributeError):
+            compute_trial_avgs(df)
+
+
+    def test_compute_trial_avgs_KeyError(self):
+        df = pd.DataFrame({'dff': [0, 1]})
+        
+        with self.assertRaises(KeyError):
+            compute_trial_avgs(df) 
+
+
+    def test_compute_trial_avgs_shape(self):
+        df = pd.DataFrame(
+            {
+                'dff': [1, 2, 3, 4],
+                'frame': [0, 1, 0, 1],
+                'stimulus': ['stim'] * 4
+            }
+        )
+        df_avg = compute_trial_avgs(df)
+        self.assertCountEqual(df_avg.shape, [2, 3])
+
+
+    def test_compute_trial_avgs_values(self):
+        df = pd.DataFrame(
+            {
+                'dff': [1, 2, 3, 4],
+                'frame': [0, 1, 0, 1],
+                'stimulus': ['stim'] * 4
+            }
+        )
+        df_avg = compute_trial_avgs(df)
+        
+        self.assertCountEqual(df_avg.stimulus, ['stim'] * 2)
+        self.assertCountEqual(df_avg.frame, [0, 1])
+        self.assertCountEqual(df_avg.dff, [2, 3])
+
+
+    def test_aggregate_cell_data_col_names_cell_ids(self):
+        dfs = [
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [1, 2, 3]
+                }
+            ),
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [2, 3, 4]
+                }
+            )
+        ]
+        dfs_merged = aggregate_cell_data(
+            dfs, 
+            cell_ids = ['001', '000'], 
+            keep_cols = ['b']
+        )
+        self.assertCountEqual(dfs_merged.columns, ['a', '001_b', '000_b'])
+
+
+    def test_aggregate_cell_data_col_names_no_cell_ids(self):
+        dfs = [
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [1, 2, 3]
+                }
+            ),
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [2, 3, 4]
+                }
+            )
+        ]
+        dfs_merged = aggregate_cell_data(dfs, keep_cols = ['b'])
+        self.assertCountEqual(dfs_merged.columns, ['a', '0_b', '1_b'])
+
+
+    def test_aggregate_cell_data_col_names_no_keep_cols(self):
+        dfs = [
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [1, 2, 3]
+                }
+            ),
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [2, 3, 4]
+                }
+            )
+        ]
+        dfs_merged = aggregate_cell_data(dfs)
+        self.assertCountEqual(dfs_merged.columns, ['a', 'b'])
+
+
+    def test_aggregate_cell_data_df_shape_keep_cols(self):
+        dfs = [
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [1, 2, 3]
+                }
+            ),
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [2, 3, 4]
+                }
+            )
+        ]
+        dfs_merged = aggregate_cell_data(dfs, keep_cols = ['b'])
+        self.assertCountEqual(dfs_merged.shape, [3, 3])
+
+
+    def test_aggregate_cell_data_df_shape_no_keep_cols(self):
+        dfs = [
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [1, 2, 3]
+                }
+            ),
+            pd.DataFrame(
+                {
+                    'a': [0, 1, 2],
+                    'b': [2, 3, 4]
+                }
+            )
+        ]
+        dfs_merged = aggregate_cell_data(dfs)
+        self.assertCountEqual(dfs_merged.shape, [6, 2])
 
 
 if __name__ == '__main__':
